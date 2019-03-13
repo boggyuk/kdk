bc0f82aa8408f86f66ec75216c0f06d80221a264
Bogdan Kowalczyk <kowalczykb@plinuk.net>
2019-03-13 13:16:49 +0000

    capture errors

--- kdk.sh
+++ kdk.sh
@@ -7,6 +7,7 @@ cmds=(
 "cat /etc/machine-id" 
 "hostnamectl"
 "kubectl get cs"
+"kubectl get pods --all-namespaces"
 "kubectl get nodes -o wide"
 "kubectl get all -o wide"
 "kubectl get ns"
@@ -24,7 +25,7 @@ for i in  "${cmds[@]}"
 
 do 
 	echo "+++++++++++++++++ $i " >> $FILE_NAME
-	$i >> $FILE_NAME
+	$i >> $FILE_NAME 2>&1
 	echo "++++++++++++++++++++++++++++++++++++++++  "  >> $FILE_NAME
 	printf "\n\n" >> $FILE_NAME 
 done
@@ -32,8 +33,11 @@ done
 
 for i in $(dpkg -l | grep kube | awk  '{print $2}')
 do 
-	apt show $i >> $FILE_NAME; 
+	apt show $i >> $FILE_NAME 2>&1; 
 done
 
 echo "+++++++++++++++++ apt/source.list " >> $FILE_NAME
 cat  /etc/apt/sources.list | grep -v '#'  >>  $FILE_NAME
+
+echo "+++++++++++++++++ recent etcd logs " >> $FILE_NAME
+sudo cat /var/log/containers/etc* | tail -n 300 >>  $FILE_NAME
